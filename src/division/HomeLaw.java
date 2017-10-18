dae6995941483c47e9188e19a08b895eb8dacc3c
9Lukas5 <Lukas.Wiest@kies-gmbh.de>
2017-10-18 19:06:53 +0200

    added HomeLaw

--- src/division/HomeLaw.java
+++ src/division/HomeLaw.java
@@ -2,42 +2,104 @@
  * Created by Classdumper, User Peter Heusch
  * Creation Date: 15.10.2017 22:12:56
  */
-
 package division;
 
-import de.hft_stuttgart.unittest.loader.Skeleton;
 import divisionData.Club;
 import divisionData.Game;
 
-@Skeleton
-public class HomeLaw {
-    static {
-        System.err.println("Please call injectClass(\"division.HomeLaw\"), you've imported a skeleton");
-    }
-
+public class HomeLaw
+{
     private static final String EXCEPTION_MSG = "Team count must be even and greater or equal to 2.";
     public static final String IDENTIC_TEAMS = "Teams cannot play against themselves";
     private final int teamCount;
     private final boolean firstRound;
 
-    public HomeLaw(int firstRound, boolean arg03) {
-        // Roughly 4 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    public HomeLaw(int teamCount, boolean firstRound)
+    {
+        if (teamCount >= 2 && teamCount % 2 == 0)
+        {
+            this.teamCount  = teamCount;
+        }
+        else
+        {
+            throw new IllegalStateException(EXCEPTION_MSG);
+        }
+
+        this.firstRound = firstRound;
     }
 
-    public Game arrangeClubs(Club team1, Club team2) {
-        // Roughly 7 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Game arrangeClubs(Club team1, Club team2)
+    {
+        if (team1.getNumber() == team2.getNumber())
+        {
+            throw new  IllegalArgumentException(IDENTIC_TEAMS);
+        }
+
+        Club joker = null;
+        if (team1.getNumber() == teamCount -1)
+        {
+            joker = team1;
+        }
+        if (team2.getNumber() == teamCount -1)
+        {
+            joker = team2;
+        }
+        if (joker != null)
+        {
+            return jokerGame(joker, (team1 == joker) ? team2 : team1);
+        }
+
+        Club smallerNumber =
+                (team1.getNumber() < team2.getNumber() ? team1 : team2);
+        Club higherNumber = ((smallerNumber == team1) ? team2 : team1);
+
+        switch((smallerNumber.getNumber() + higherNumber.getNumber()) % 2)
+        {
+            case 0:
+                return regularGame(smallerNumber, higherNumber);
+            case 1:
+                return regularGame(higherNumber, smallerNumber);
+        }
+
+        return null;
     }
 
-    private Game jokerGame(Club team1, Club team2) {
-        // Roughly 1 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    private Game jokerGame(Club joker, Club team2)
+    {
+        if (team2.getNumber() >= (teamCount / 2) -2 && team2.getNumber() <= teamCount -3)
+        {
+            if (firstRound)
+            {
+                return new Game(joker, team2);
+            }
+            else
+            {
+                return new Game(team2, joker);
+            }
+        }
+        else
+        {
+            if(firstRound)
+            {
+                return new Game(team2, joker);
+            }
+            else
+            {
+                return new Game(joker, team2);
+            }
+        }
     }
 
-    private Game regularGame(Club team1, Club team2) {
-        // Roughly 1 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    private Game regularGame(Club team1, Club team2)
+    {
+        if(!firstRound)
+        {
+            return new Game(team1, team2);
+        }
+        else
+        {
+            return new Game(team2, team1);
+        }
     }
+
 }
-
