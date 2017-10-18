f6c3209ece8ee44b291c385abf3c2b5f8baf1782
9Lukas5 <Lukas.Wiest@kies-gmbh.de>
2017-10-18 19:09:09 +0200

    add Shifter

--- src/divisionData/Shifter.java
+++ src/divisionData/Shifter.java
@@ -2,37 +2,52 @@
  * Created by Classdumper, User Peter Heusch
  * Creation Date: 15.10.2017 22:12:55
  */
-
 package divisionData;
 
-import de.hft_stuttgart.unittest.loader.Skeleton;
-
-@Skeleton
-public class Shifter {
-    static {
-        System.err.println("Please call injectClass(\"divisionData.Shifter\"), you've imported a skeleton");
-    }
+public class Shifter
+{
 
     private final State state;
 
-    public Shifter(State arg02) {
-        // Roughly 1 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Shifter(State state)
+    {
+        this.state = state;
     }
 
-    public void update(boolean oddDay) {
-        // Roughly 2 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void update(boolean oddDay)
+    {
+        if (oddDay)
+        {
+            System.out.println("Day is odd, but don't know why^^");
+        }
+        else
+        {
+            System.out.println("normal day");
+        }
     }
 
-     Club pushBack(Club newClub, Club[] state) {
-        // Roughly 2 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    Club pushBack(Club newClub, Club[] state)
+    {
+        Club fallout = state[0];
+        for(int i=0; i < state.length -1; i++)
+        {
+            state[0] = state[i+1];
+        }
+        state[state.length -1] = newClub;
+
+        return fallout;
     }
 
-     Club pushFront(Club newClub, Club[] state) {
-        // Roughly 2 lines of implementation
-        throw new UnsupportedOperationException("Not yet implemented");
+    Club pushFront(Club newClub, Club[] state)
+    {
+        Club fallout = state[state.length -1];
+        for(int i=state.length -2; i >= 0; i--)
+        {
+            state[i+1] = state[i];
+        }
+        state[0] = newClub;
+
+        return fallout;
     }
+
 }
-
